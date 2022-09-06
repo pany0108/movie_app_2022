e3eaa073f592950cc9530c246b906dc59ab5791b
Nayoung Park <nyp@nuritelecom.com>
2022-09-06 15:15:06 +0900

    라우터 초기 설정

--- src/App.js
+++ src/App.js
@@ -1,54 +1,9 @@
-import axios from 'axios';
-import React, { Component } from 'react';
-import Movie from './Movie';
+import React from 'react';
+import Home from './routes/Home';
 import './App.css';
 
-class App extends Component {
-  state = {
-    isLoading: true,
-    movies: [],
-  };
-
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies },
-      },
-    } = await axios.get('https://yts.mx/api/v2/list_movies.json?sort_by=rating');
-    this.setState({ movies, isLoading: false });
-  };
-
-  componentDidMount() {
-    // 영화 데이터 로딩!
-    this.getMovies();
-  }
-
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader_text">Loading...</span>
-          </div>
-        ) : (
-          <div className="movies">
-            {movies.map((movie) => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                title={movie.title}
-                year={movie.year}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-              />
-            ))}
-          </div>
-        )}
-      </section>
-    );
-  }
+function App() {
+  return <Home />;
 }
 
 export default App;
