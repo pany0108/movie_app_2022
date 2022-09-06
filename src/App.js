baf42232fba49a0ee9ba091565b4baff516adf51
Nayoung Park <nyp@nuritelecom.com>
2022-09-06 15:01:51 +0900

    스타일 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import React, { Component } from 'react';
+import Movie from './Movie';
+import './App.css';
 
 class App extends Component {
   state = {
@@ -8,7 +10,12 @@ class App extends Component {
   };
 
   getMovies = async () => {
-    const movies = await axios.get('https://yts.mx/api/v2/list_movies.json');
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get('https://yts.mx/api/v2/list_movies.json?sort_by=rating');
+    this.setState({ movies, isLoading: false });
   };
 
   componentDidMount() {
@@ -17,8 +24,30 @@ class App extends Component {
   }
 
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? 'Loading...' : 'Ready'}</div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader_text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                title={movie.title}
+                year={movie.year}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
   }
 }
 
