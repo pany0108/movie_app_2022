9cefcc946e627064648f6a8abf589379832fd34a
Nayoung Park <nyp@nuritelecom.com>
2022-09-06 13:45:11 +0900

    영화 api 받아오기

--- src/App.js
+++ src/App.js
@@ -1,63 +1,25 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import axios from 'axios';
+import React, { Component } from 'react';
 
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>I Like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
+class App extends Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+
+  getMovies = async () => {
+    const movies = await axios.get('https://yts.mx/api/v2/list_movies.json');
+  };
+
+  componentDidMount() {
+    // 영화 데이터 로딩!
+    this.getMovies();
+  }
+
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? 'Loading...' : 'Ready'}</div>;
+  }
 }
-
-const foodLike = [
-  {
-    id: 1,
-    name: '김치',
-    image: 'https://health.chosun.com/site/data/img_dir/2021/09/01/2021090100998_0.jpg',
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: '삼겹살',
-    image: 'https://src.hidoc.co.kr/image/lib/2021/8/27/1630049987719_0.jpg',
-    rating: 4.9,
-  },
-  {
-    id: 3,
-    name: '비빔밥',
-    image: 'https://health.chosun.com/site/data/img_dir/2021/01/27/2021012702508_0.jpg',
-    rating: 3.0,
-  },
-  {
-    id: 4,
-    name: '돈까스',
-    image: 'https://recipe1.ezmember.co.kr/cache/recipe/2019/01/12/b9343d314206275c1b6d0d0c4fcc2ce71.jpg',
-    rating: 4.0,
-  },
-  {
-    id: 5,
-    name: '김밥',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Gimbap_%28pixabay%29.jpg',
-    rating: 2.5,
-  },
-];
-
-function App() {
-  return (
-    <div>
-      {foodLike.map((dish) => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
-      ))}
-    </div>
-  );
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-};
 
 export default App;
